465d06ebe495c094fe542d4b8478358b82070562
Przemek <krepski80@gmail.com>
2023-09-13 19:56:34 +0200

    commit  before styling

---
+++ study-buddy/src/data/users.js
@@ -0,0 +1,27 @@
+export const users = [
+  {
+    name: 'Adam Romański',
+    attendance: '39%',
+    average: '3.3',
+  },
+  {
+    name: 'Marcin Romański',
+    attendance: '40%',
+    average: '23',
+  },
+  {
+    name: 'Paweł Romański',
+    attendance: '35%',
+    average: '5.3',
+  },
+  {
+    name: 'Konrad Romański',
+    attendance: '53%',
+    average: '1.3',
+  },
+  {
+    name: 'Bogumil Romański',
+    attendance: '29%',
+    average: '7.3',
+  },
+];
